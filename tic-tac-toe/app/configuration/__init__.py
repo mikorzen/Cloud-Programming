7c1c54c145ae56f9a27ea7ce7d9d36e1e349d5eb
mikorzen <korzenmichal5@gmail.com>
2024-03-21 12:21:08 +0100

    move static file definition to configuration and remove Draw handling from main (it's handled in game manager)

--- tic-tac-toe/app/configuration/__init__.py
+++ tic-tac-toe/app/configuration/__init__.py
@@ -1,7 +1,8 @@
-# ruff:noqa:ERA001
+# ruff: noqa: ERA001
 
 from pathlib import Path
 
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 __all__ = [
@@ -11,7 +12,12 @@ __all__ = [
 # Define paths to the configuration file and the template directory
 # (.parents[i] is equivalent to calling .parent i+1 times)
 _CONFIG_FILE = Path(__file__).resolve().parents[1] / "config.toml"
+_STATIC_DIR = Path(__file__).resolve().parents[2] / "static"
 _TEMPLATE_DIR = Path(__file__).resolve().parents[2] / "templates"
+
+# Bind the static directory to a StaticFiles object
+# that will be used to serve static files on request
+static_files = StaticFiles(directory=_STATIC_DIR)
 
 # Bind the template directory to a Jinja2Templates object
 # that will be used to serve templates on request
--- tic-tac-toe/app/main.py
+++ tic-tac-toe/app/main.py
@@ -1,16 +1,9 @@
 # ruff: noqa: F401, ERA001
 
-from pathlib import Path
-
-from configuration import templates
+from configuration import static_files, templates
 from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
 from logic.manager import GameFullError, GameManager, GameOver, InvalidMoveError
-from logic.tictactoe import Draw
-
-_STATIC_DIR = Path(__file__).resolve().parents[1] / "static"
-static_files = StaticFiles(directory=_STATIC_DIR)
 
 app = FastAPI()
 app.mount("/static", app=static_files, name="static")
@@ -47,8 +40,6 @@ async def websocket_endpoint(websocket: WebSocket) -> None:
 
     try:
         await game_manager.run_game(websocket)
-    except Draw as e:
-        await websocket.send_json({"error": str(e)})
     except InvalidMoveError as e:
         await websocket.send_json({"error": str(e)})
     except GameOver:
