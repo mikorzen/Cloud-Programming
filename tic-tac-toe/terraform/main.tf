117c84d3b990012f23e383e3ea3987674e24b1f5
mikorzen <korzenmichal5@gmail.com>
2024-04-10 15:29:59 +0200

    lab3 done

--- tic-tac-toe/terraform/main.tf
+++ tic-tac-toe/terraform/main.tf
@@ -1,91 +1,59 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
     }
   }
 }
 
 
 provider "aws" {
-  region = var.region  # Ustawienie regionu instancji
+  region = var.region # Ustawienie regionu instancji
 }
 
 
-resource "aws_key_pair" "tf_key" {
-  key_name   = var.key_name  # Ustawienie nazwy dla klucza SSH
-  public_key = tls_private_key.rsa.public_key_openssh  # Ustawienie klucza publicznego
+resource "aws_ecr_repository" "ecr" {
+  name = "tictactoe-repo" # Utworzenie repozytorium ECR (Elastic Container Registry)
 }
 
 
-resource "aws_vpc" "vpc" {
-  cidr_block           = var.cidr_block
-  enable_dns_hostnames = true  # Włączenie hostname'ów dla DNS
-  enable_dns_support   = true  # Włączenie DNS
-}
-
-
-resource "aws_subnet" "subnet" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = cidrsubnet(aws_vpc.vpc.cidr_block, 8, 1)  # Podział CIDR VPC na podsieć
-  availability_zone       = var.availability_zone  # Wybór strefy dostępności (us-east-1a)
-  map_public_ip_on_launch = true
-}
-
-
-resource "aws_internet_gateway" "gateway" {  # Utworzenie bramy do komunikacji z Internetem
-  vpc_id = aws_vpc.vpc.id
-}
-
-
-resource "aws_route_table" "route" {  # Utworzenie tablicy routingu
-  vpc_id = aws_vpc.vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"  # Ustawienie trasy dla wszystkich przez bramę
-    gateway_id = aws_internet_gateway.gateway.id
-  }
-}
-
-
-resource "aws_main_route_table_association" "association" {  # Ustawienie stworzonej tablicy routingu jako domyślnej
-  vpc_id         = aws_vpc.vpc.id
-  route_table_id = aws_route_table.route.id
+data "aws_vpc" "vpc" {
+  default = true # Ustawienie domyślnej sieci VPC
 }
 
 
 resource "aws_security_group" "security" {
-  vpc_id = aws_vpc.vpc.id
+  vpc_id = data.aws_vpc.vpc.id
 
-  ingress {  # Zezwolenie na ruch przychodzący na port 22 (SSH)
+  ingress { # Zezwolenie na ruch przychodzący na port 22 (SSH)
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {  # Zezwolenie na ruch przychodzący na port 80 (HTTP)
+  ingress { # Zezwolenie na ruch przychodzący na port 80 (HTTP)
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {  # Zezwolenie na ruch przychodzący na port 443 (HTTPS)
+  ingress { # Zezwolenie na ruch przychodzący na port 443 (HTTPS)
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {  # Zezwolenie na ruch przychodzący na port 8080 (aplikacja)
+  ingress { # Zezwolenie na ruch przychodzący na port 8080 (aplikacja)
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {  # Zezwolenie na ruch wychodzący na wszystkie porty
+  egress { # Zezwolenie na ruch wychodzący na wszystkie porty
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
@@ -93,15 +61,56 @@ resource "aws_security_group" "security" {
   }
 }
 
-resource "aws_instance" "instance" {
-  ami                         = var.image_id  # Wybór obrazu AMI dla maszyny (Amazon Linux 2023)
-  instance_type               = var.instance_type  # Wybór typu instancji (t2.micro, jedyna opcja dostępna dla free tier)
-  subnet_id                   = aws_subnet.subnet.id  # Przypisanie instancji do podsieci
-  vpc_security_group_ids      = [aws_security_group.security.id]  # Przypisanie grupy reguł bezpieczeństwa
-  key_name                    = var.key_name  # Przypisanie klucza SSH
-  associate_public_ip_address = true  # Ustawienie przydzielenia publicznego adresu IP
 
-  tags = {
-    Name = var.instance_name  # Ustawienie nazwy instancji
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = "tictactoe-cluster" # Utworzenie klastra ECS (Elastic Container Service)
+}
+
+
+resource "aws_ecs_task_definition" "ecs_task" {
+  family                   = var.ecs_task_name    # Utworzenie definicji zadania ECS
+  network_mode             = var.ecs_network_mode # Ustawienie trybu sieciowego
+  requires_compatibilities = [var.ecs_type]       # Ustawienie kompatybilności z Fargate
+
+  execution_role_arn = "arn:aws:iam::861864057306:role/LabRole" # Ustawienie roli wykonawczej dla zadania
+
+  container_definitions = jsonencode([ # Konfiguracja kontenera
+    {
+      name   = var.ecs_container_name
+      image  = "${aws_ecr_repository.ecr.repository_url}:latest" # Ustawienie obrazu Dockerowego
+      cpu    = var.ecs_cpu                                       # Ustawienie CPU
+      memory = var.ecs_memory                                    # Ustawienie pamięci
+      port_mappings = [{                                         # Mapowanie portów
+        container_port = 8080
+        host_port      = 8080
+        protocol       = "tcp"
+      }]
+    }
+  ])
+
+  cpu    = var.ecs_cpu    # Ustawienie CPU dla całego zadania
+  memory = var.ecs_memory # Ustawienie pamięci dla całego zadania
+}
+
+
+data "aws_subnets" "subnets" { # Pobranie informacji o podsieciach w VPC
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.vpc.id]
+  }
+}
+
+
+resource "aws_ecs_service" "ecs_service" {
+  name            = var.ecs_service_name
+  cluster         = aws_ecs_cluster.ecs_cluster.id       # Ustawienie klastra ECS dla serwisu
+  task_definition = aws_ecs_task_definition.ecs_task.arn # Ustawienie definicji zadania dla serwisu
+  desired_count   = var.ecs_service_instance_count       # Ustawienie liczby instancji kontenera
+  launch_type     = var.ecs_type                         # Ustawienie typu uruchomienia kontenera
+
+  network_configuration { # Konfiguracja sieciowa serwisu
+    subnets          = data.aws_subnets.subnets.ids
+    security_groups  = [aws_security_group.security.id]
+    assign_public_ip = true
   }
 }
--- tic-tac-toe/terraform/variables.tf
+++ tic-tac-toe/terraform/variables.tf
@@ -1,47 +1,78 @@
-variable key_name {
-    description = "Name of the key pair"
-    type        = string
-    default     = "tf_key"
+variable "key_name" {
+  description = "Name of the key pair"
+  type        = string
+  default     = "tf_key"
 }
 
 variable "key_file" {
-    description = "Name of the key file"
-    type        = string
-    default     = "tf_key.pem"
+  description = "Name of the key file"
+  type        = string
+  default     = "tf_key.pem"
 }
 
-variable region {
-    description = "AWS region"
-    type        = string
-    default     = "us-east-1"
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
 }
 
-variable image_id {
-    description = "AMI ID"
-    type        = string
-    default     = "ami-051f8a213df8bc089"
+variable "ecr_name" {
+  description = "Name of the ECR repository"
+  type        = string
+  default     = "tictactoe-repo"
 }
 
-variable instance_type {
-    description = "Instance type"
-    type        = string
-    default     = "t2.micro"
+variable "ecs_cluster_name" {
+  description = "Name of the ECS cluster"
+  type        = string
+  default     = "tictactoe-cluster"
 }
 
-variable instance_name {
-    description = "Name of the instance"
-    type        = string
-    default     = "TicTacToe"
+variable "ecs_task_name" {
+  description = "Name of the ECS task"
+  type        = string
+  default     = "tictactoe-task"
 }
 
-variable availability_zone {
-    description = "Availability zone"
-    type        = string
-    default     = "us-east-1a"
+variable "ecs_network_mode" {
+  description = "Network mode for the ECS task"
+  type        = string
+  default     = "awsvpc"
 }
 
-variable cidr_block {
-    description = "CIDR block for the VPC"
-    type        = string
-    default     = "10.0.0.0/16"
+variable "ecs_type" {
+  description = "Type of the ECS task"
+  type        = string
+  default     = "FARGATE"
+}
+
+variable "ecs_container_name" {
+  description = "Name of the ECS container"
+  type        = string
+  default     = "tictactoe-container"
+}
+
+
+variable "ecs_cpu" {
+  description = "CPU units for the ECS task"
+  type        = number
+  default     = 256
+}
+
+variable "ecs_memory" {
+  description = "Memory for the ECS task"
+  type        = number
+  default     = 512
+}
+
+variable "ecs_service_name" {
+  description = "Name of the ECS service"
+  type        = string
+  default     = "tictactoe-service"
+}
+
+variable "ecs_service_instance_count" {
+  description = "Number of instances for the ECS service"
+  type        = number
+  default     = 1
 }
